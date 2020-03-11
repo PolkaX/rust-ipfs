48cdcbee3a7f033cb1e8e10b52b1bd02ace9050d
koushiro <koushiro.cqx@gmail.com>
2020-03-11 15:53:07 +0800

    Replace cid with the fork version; Fix block-format
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- core/block-format/src/basic_block.rs
+++ core/block-format/src/basic_block.rs
@@ -2,8 +2,8 @@
 
 use bytes::Bytes;
 
-use cid::{AsCidRef, Cid, Multihash};
-use util::sha2_256_hash;
+use cid::{AsCidRef, Cid};
+use multihash::{MultihashRef, Sha2_256};
 
 use crate::error::Result;
 
@@ -11,6 +11,18 @@ use crate::error::Result;
 pub trait Block: AsCidRef {
     /// Get the raw data of block.
     fn raw_data(&self) -> &Bytes;
+}
+
+impl Block for BasicBlock {
+    fn raw_data(&self) -> &Bytes {
+        &self.data
+    }
+}
+
+impl AsCidRef for BasicBlock {
+    fn cid(&self) -> &Cid {
+        &self.cid
+    }
 }
 
 /// The basic block.
@@ -23,10 +35,10 @@ pub struct BasicBlock {
 impl BasicBlock {
     /// Creates a new `BasicBlock` with given bytes, and its CID is version 0.
     pub fn new(data: Bytes) -> BasicBlock {
-        let sha256_hash = sha2_256_hash(data.as_ref());
+        let sha256_hash = Sha2_256::digest(data.as_ref());
         BasicBlock {
             data,
-            cid: Cid::new_cid_v0(sha256_hash).expect("invalid hash for CIDv0"),
+            cid: Cid::new_v0(sha256_hash).expect("invalid hash for CIDv0"),
         }
     }
 
@@ -35,10 +47,7 @@ impl BasicBlock {
         #[cfg(debug_assertions)]
         {
             use crate::error::BlockFormatError;
-            let checked_cid = cid
-                .prefix()
-                .sum(data.as_ref())
-                .map_err(BlockFormatError::CidError)?;
+            let checked_cid = Cid::new_from_prefix(&cid.prefix(), data.as_ref());
             if checked_cid != cid {
                 return Err(BlockFormatError::WrongHash(checked_cid, cid));
             }
@@ -48,20 +57,8 @@ impl BasicBlock {
     }
 
     /// Get the multihash of cid of the basic block.
-    pub fn multihash(&self) -> Multihash {
-        self.cid.multihash()
-    }
-}
-
-impl Block for BasicBlock {
-    fn raw_data(&self) -> &Bytes {
-        &self.data
-    }
-}
-
-impl AsCidRef for BasicBlock {
-    fn cid(&self) -> &Cid {
-        &self.cid
+    pub fn multihash(&self) -> MultihashRef {
+        self.cid.hash()
     }
 }
 
